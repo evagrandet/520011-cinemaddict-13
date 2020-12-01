66713819bccc66c0f3f4e52e569644ce08d821e9
evagrandet <evagrandet@ro.ru>
2020-12-01 22:38:39 +0300

    Выполнение задания

---
+++ src/view/show-more-btn-view.js
@@ -0,0 +1,27 @@
+import {createElement} from '../util';
+
+export const createShowMoreButtonTemplate = () => {
+  return `<button class="films-list__show-more">Show more</button>`;
+};
+
+export default class ShowMoreBtnView {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createShowMoreButtonTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
