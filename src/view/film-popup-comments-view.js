706df43c7cb6474efafb542ca26258b7e866827c
evagrandet <evagrandet@ro.ru>
2021-01-14 01:37:53 +0300

    Добавила обработку he текста комментария

--- src/view/film-popup-comments-view.js
+++ src/view/film-popup-comments-view.js
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import he from "he";
 import relativeTime from 'dayjs/plugin/relativeTime';
 import AbstractSmartView from './abstract-smart-view';
 
@@ -11,7 +12,7 @@ const createCommentTemplate = (comment) => {
         <img src="./images/emoji/${emoji}.png" width="55" height="55" alt="emoji-${emoji}">
       </span>
       <div>
-        <p class="film-details__comment-text">${text}</p>
+        <p class="film-details__comment-text">${he.encode(text)}</p>
         <p class="film-details__comment-info">
           <span class="film-details__comment-author">${author}</span>
           <span class="film-details__comment-day">${dayjs(date).fromNow()}</span>
