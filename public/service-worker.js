bbc0219d25498dc61a4845682584b4ba63554e5b
evagrandet <evagrandet@ro.ru>
2021-01-30 23:05:02 +0300

    Правки в сервис-воркере

--- public/service-worker.js
+++ public/service-worker.js
@@ -38,15 +38,14 @@ const CASHED_ASSETS = [
 
 self.addEventListener(`install`, (evt) => {
   evt.waitUntil(
-      caches.open(CACHE_NAME)
-        .then((cache) => cache.addAll(CASHED_ASSETS))
-  );
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(CASHED_ASSETS);
+      }));
 });
 
 self.addEventListener(`activate`, (evt) => {
   evt.waitUntil(
-      caches.keys()
-      .then((keys) => Promise.all(
+      caches.keys().then((keys) => Promise.all(
           keys.map((key) => {
             if (key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME) {
               return caches.delete(key);
@@ -62,21 +61,18 @@ const handleFetch = (evt) => {
   const {request} = evt;
 
   evt.respondWith(
-      caches.match(request)
-      .then((cacheResponse) => {
+      caches.match(request).then((cacheResponse) => {
         if (cacheResponse) {
           return cacheResponse;
         }
-        return fetch(request)
-          .then((response) => {
-            if (!response || response.status !== HTTP_STATUS_OK || response.type !== RESPONSE_SAFE_TYPE) {
-              return response;
-            }
-            const clonedResponse = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => cache.put(request, clonedResponse));
+        return fetch(request).then((response) => {
+          if (!response || response.status !== HTTP_STATUS_OK || response.type !== RESPONSE_SAFE_TYPE) {
             return response;
-          });
+          }
+          const clonedResponse = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, clonedResponse));
+          return response;
+        });
       })
   );
 };
